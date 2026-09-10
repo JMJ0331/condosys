4efa30638cd93ec41135ae25a520e8c946937cf5
Carlos Acosta Rosario <carlosacosta0637@gmail.com>
2026-09-09 20:54:05 -0400

    sigo luego

--- condosys/condosys/settings.py
+++ condosys/condosys/settings.py
@@ -16,7 +16,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
--- condosys/inicio/views.py
+++ condosys/inicio/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-@login_required
+
 def inicio(request):
     return render(request, 'inicio/index.html')
