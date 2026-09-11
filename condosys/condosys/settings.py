b564b673b684a4807e0e3dd13c0f51ae4427166d
Carlos Acosta <carlosacosta0637@gmail.com>
2026-09-11 16:13:45 -0400

    update: configuro django-filter y el directorio de media

--- condosys/condosys/settings.py
+++ condosys/condosys/settings.py
@@ -181,6 +181,10 @@ STATICFILES_DIRS = [
 # Directory where `collectstatic` will collect static files for production
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SECURE_HSTS_SECONDS = 31536000
@@ -208,6 +212,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
