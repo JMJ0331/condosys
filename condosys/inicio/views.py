8c0d3cf97f33b228d5be10abf037499c7708efd6
Carlos Acosta Rosario <carlosacosta0637@gmail.com>
2026-09-10 22:09:09 -0400

    update: agregue las acciones del apartado de acciones rápidas

--- condosys/inicio/views.py
+++ condosys/inicio/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from incidents.forms import IncidentForm
+from payments.forms import PaymentForm
+from visitors.forms import VisitorForm
+
 # Create your views here.
 
-
 def inicio(request):
-    return render(request, 'inicio/index.html')
+    contexto = {
+        'payment_form': PaymentForm(),
+        'visitor_form': VisitorForm(),
+        'incident_form': IncidentForm(),
+    }
+    return render(request, 'inicio/index.html', contexto)
