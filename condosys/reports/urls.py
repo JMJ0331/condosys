ce66ad3c51af66b87d081428bca7d82501fa1887
Carlos Acosta <carlosacosta0637@gmail.com>
2026-09-11 16:14:04 -0400

    add: formularios POST de registro (crear_*) en reports y structure

--- condosys/reports/urls.py
+++ condosys/reports/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AuditLogViewSet, ReportViewSet, app_index
+from .views import AuditLogViewSet, ReportViewSet, app_index, crear_audit_log, crear_audit_log_detail
 
 router = DefaultRouter()
 router.register(r'audit-logs', AuditLogViewSet, basename='auditlog')
 
 urlpatterns = [
+    path('crear/audit-log/', crear_audit_log, name='crear_audit_log'),
+    path('crear/detalle/', crear_audit_log_detail, name='crear_audit_log_detail'),
     path('', app_index, name='reports_index'),
     path('', include(router.urls)),
     path('summary/', ReportViewSet.as_view({'get': 'summary'}), name='report-summary'),
--- condosys/structure/urls.py
+++ condosys/structure/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GardenViewSet, BuildingViewSet, ApartmentViewSet, app_index
+from .views import (
+    GardenViewSet, BuildingViewSet, ApartmentViewSet, app_index,
+    crear_jardin, crear_edificio, crear_departamento,
+)
 
 router = DefaultRouter()
 router.register(r'gardens', GardenViewSet, basename='garden')
@@ -8,6 +11,9 @@ router.register(r'buildings', BuildingViewSet, basename='building')
 router.register(r'apartments', ApartmentViewSet, basename='apartment')
 
 urlpatterns = [
+    path('crear/jardin/', crear_jardin, name='crear_jardin'),
+    path('crear/edificio/', crear_edificio, name='crear_edificio'),
+    path('crear/departamento/', crear_departamento, name='crear_departamento'),
     path('', app_index, name='structure_index'),
     path('', include(router.urls)),
 ]
