097b7e6d95267c63b53a7b15bb4a1081dab5e468
Carlos Acosta Rosario <carlosacosta0637@gmail.com>
2026-09-10 22:09:43 -0400

    update: actualize la config

--- condosys/condosys/settings.py
+++ condosys/condosys/settings.py
@@ -19,7 +19,7 @@ load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
